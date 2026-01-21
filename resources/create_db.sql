d89fbe9ddbe6828324028431e581a28e8a0d47be
Amrutha S <amruthas207@gmail.com>
2026-01-21 13:44:39 +0000

    yes

---
+++ resources/create_db.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS cases (
+   case_number TEXT PRIMARY KEY,
+   name TEXT,
+   subject TEXT,
+   status TEXT,
+   date_time TEXT
+);
+
+DELETE FROM cases;
+
+INSERT INTO cases (
+   case_number,
+   name,
+   subject,
+   status,
+   date_time
+) VALUES (
+   '00001002',
+   'SKood',
+   'Caso de ejemplo 3: no es posible realizar un seguimiento de nuestro pedido',
+   'En espera',
+   '12/22/2025 7:09 AM'
+);
